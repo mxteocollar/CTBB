3e5c4c5c878a31636c8e05b39b93d648a9d0f622
mxteocollar <collarmateo0610@gmail.com>
2025-08-07 21:20:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -770,12 +770,12 @@ function updateChestUI_Modal() {
     chestCooldownModal.textContent = '';
     openChestBtnModal.disabled = false;
     chestImgModal.style.cursor = 'pointer';
-    chestImgModal.src = '/assets/closedChest.png';
+    chestImgModal.src = '/CTBB/assets/closedChest.png';
   } else {
     chestCooldownModal.textContent = `Cooldown: ${remaining}s`;
     openChestBtnModal.disabled = true;
     chestImgModal.style.cursor = 'not-allowed';
-    chestImgModal.src = '/assets/closedChest.png';
+    chestImgModal.src = '/CTBB/assets/closedChest.png';
   }
 }
 
@@ -787,11 +787,11 @@ function openChest_Modal() {
   chestImgModal.classList.add('opening');
   openChestBtnModal.disabled = true;
   chestImgModal.style.cursor = 'not-allowed';
-  chestImgModal.src = '/assets/openedChest.png';
+  chestImgModal.src = '/CTBB/assets/openedChest.png';
 
   setTimeout(() => {
     chestImgModal.classList.remove('opening');
-    chestImgModal.src = '/assets/closedChest.png';
+    chestImgModal.src = '/CTBB/assets/closedChest.png';
 
     const roll = Math.random() * 100;
 
@@ -867,3 +867,4 @@ function getCooldownRemaining() {
 }
 
 });
+
